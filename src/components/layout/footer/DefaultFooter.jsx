f8231153b443c9ea8a2fadb5d1117e6db9c18c63
devskinshy <yyshgk12@gmail.com>
2024-08-27 22:42:16 +0900

    layout header settings

---
+++ src/components/layout/footer/DefaultFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DefaultFooter() {
+	return (
+		<footer>
+			<div className='container mx-auto px-4 py-8'>footer</div>
+		</footer>
+	);
+}
+
+export default DefaultFooter;
