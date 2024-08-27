4b86501e09b5a4d0b99843e47e0ed66c702a5bd2
devskinshy <yyshgk12@gmail.com>
2024-08-27 22:42:16 +0900

    layout header settings

---
+++ src/components/layout/header/DefaultHeader.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { Globe, AlignLeft } from 'lucide-react';
+import { useTranslations } from 'next-intl';
+
+function DefaultHeader() {
+	const t = useTranslations('header');
+
+	return (
+		<header>
+			<div className='container mx-auto px-4 py-8'>
+				<div className='flex items-center justify-between'>
+					<div className='h-12 w-36'>
+						<Image
+							src='/ko/ico-logo.png'
+							alt='logo'
+							width={201}
+							height={52}
+							layout='responsive'
+						/>
+					</div>
+
+					<div className='flex items-center'>
+						<Button variant='secondary'>{t('login')}</Button>
+						<Button variant='ghost' size='icon'>
+							<Globe />
+						</Button>
+						<Button variant='ghost' size='icon'>
+							<AlignLeft />
+						</Button>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default DefaultHeader;
