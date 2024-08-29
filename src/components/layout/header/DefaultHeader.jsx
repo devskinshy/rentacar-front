cd1bf2e45b3bb142b388efcea69da61e37f9e771
devskinshy <yyshgk12@gmail.com>
2024-08-29 23:50:48 +0900

    layout header settings

--- src/components/layout/header/DefaultHeader.jsx
+++ src/components/layout/header/DefaultHeader.jsx
@@ -3,6 +3,22 @@ import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Globe, AlignLeft } from 'lucide-react';
 import { useTranslations } from 'next-intl';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+	DropdownMenuPortal,
+} from '@/components/ui/dropdown-menu';
+
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from '@/components/ui/sheet';
 
 function DefaultHeader() {
 	const t = useTranslations('header');
@@ -21,14 +37,37 @@ function DefaultHeader() {
 						/>
 					</div>
 
-					<div className='flex items-center space-x-1'>
+					<div className='flex items-center space-x-3'>
 						<Button variant='secondary'>{t('login')}</Button>
-						<Button variant='ghost' size='icon'>
-							<Globe />
-						</Button>
-						<Button variant='ghost' size='icon'>
-							<AlignLeft />
-						</Button>
+
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Globe />
+							</DropdownMenuTrigger>
+							<DropdownMenuPortal>
+								<DropdownMenuContent>
+									<DropdownMenuItem>한국어</DropdownMenuItem>
+									<DropdownMenuItem>영어</DropdownMenuItem>
+									<DropdownMenuItem>일본어</DropdownMenuItem>
+									<DropdownMenuItem>중국어</DropdownMenuItem>
+								</DropdownMenuContent>
+							</DropdownMenuPortal>
+						</DropdownMenu>
+
+						<Sheet>
+							<SheetTrigger asChild>
+								<AlignLeft />
+							</SheetTrigger>
+							<SheetContent>
+								<SheetHeader>
+									<SheetTitle>Are you absolutely sure?</SheetTitle>
+									<SheetDescription>
+										This action cannot be undone. This will permanently delete
+										your account and remove your data from our servers.
+									</SheetDescription>
+								</SheetHeader>
+							</SheetContent>
+						</Sheet>
 					</div>
 				</div>
 			</div>
