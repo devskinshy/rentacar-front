5995233081e01727c0c8dd573a15bd576f0f85eb
devskinshy <yyshgk12@gmail.com>
2024-08-31 16:32:00 +0900

    layout footer settings

--- src/components/layout/footer/DefaultFooter.jsx
+++ src/components/layout/footer/DefaultFooter.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import { useTranslations } from 'next-intl';
 
 function DefaultFooter() {
+	const t = useTranslations('header');
+
 	return (
 		<footer className='bg-black'>
 			<div className='container mx-auto px-4 py-8'>
 				<div className='flex items-center justify-between text-white'>
-					<div className='flex h-5 items-center space-x-4'>
+					<div className='flex h-3 items-center space-x-4'>
 						<div>개인정보처리방침</div>
 						<Separator orientation='vertical' />
 						<div>이용약관</div>
@@ -15,7 +18,8 @@ function DefaultFooter() {
 					<div>
 						<Button
 							variant='outline'
-							className='border-background bg-inherit text-inherit'>
+							className='rounded- rounded-full border-background bg-inherit text-inherit'
+						>
 							ENGLISH
 						</Button>
 					</div>
