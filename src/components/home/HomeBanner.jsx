34811242f823dc11c6a6b6f44de39615e6666478
devskinshy <yyshgk12@gmail.com>
2024-09-06 23:39:37 +0900

    layout page settings

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import Image from 'next/image';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+} from '@/components/ui/carousel';
 
 function HomeBanner() {
 	return (
 		<section>
-			<Image
-				src='https://www.lotterentacar.net/atch/getImage.do?atchFileId=FILE0000000004199161'
-				alt='banner'
-				width={1308}
-				height={560}
-			/>
+			<Carousel className='w-full'>
+				<CarouselContent>
+					{Array.from({ length: 5 }).map((_, index) => (
+						<CarouselItem key={index}>
+							<Card>
+								<CardContent className='overflow-hidden rounded-2xl p-0'>
+									<Image
+										src='https://www.lotterentacar.net/atch/getImage.do?atchFileId=FILE0000000004199161'
+										alt='banner'
+										width={1308}
+										height={560}
+									/>
+								</CardContent>
+							</Card>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+			</Carousel>
 		</section>
 	);
 }
